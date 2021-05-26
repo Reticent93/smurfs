902a01f655d0394824eca4daa642203b888cf7d7
Greg <remph819@gmail.com>
2021-05-25 17:36:51 -0700

    updated functions directory

--- functions/post.js
+++ functions/post.js
@@ -6,7 +6,7 @@ exports.handler = async function (event, context) {
     const server = express();
     server.use(express.json());
     // server.use(cors());
-    try {
+
         const sendUserError = (msg, res) => {
             res.status(422);
             res.json({Error: msg});
@@ -22,58 +22,75 @@ exports.handler = async function (event, context) {
             }
         ];
         server.get('/smurfs', (req, res) => {
-            res.json(smurfs);
+            try {   res.json(smurfs);
+            }catch (e) {
+                console.error(e)
+            }
         });
         let smurfId = smurfs.length;
 
         server.post('/smurfs', (req, res) => {
-            const {name, age, height} = req.body;
-            const newSmurf = {name, age, height, id: smurfId};
-            if (!name || !age || !height) {
-                return sendUserError(
-                    'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
-                    res
-                );
-            }
-            const findSmurfByName = (smurf) => {
-                return smurf.name === name;
-            };
-            if (smurfs.find(findSmurfByName)) {
-                return sendUserError(`Ya gone did smurfed! ${name} already exists in the smurf DB.`, res);
-            }
+            try {
+                const {name, age, height} = req.body;
+                const newSmurf = {name, age, height, id: smurfId};
+                if (!name || !age || !height) {
+                    return sendUserError(
+                        'Ya gone did smurfed! Name/Age/Height are all required to create a smurf in the smurf DB.',
+                        res
+                    );
+                }
+                const findSmurfByName = (smurf) => {
+                    return smurf.name === name;
+                };
+                if (smurfs.find(findSmurfByName)) {
+                    return sendUserError(`Ya gone did smurfed! ${name} already exists in the smurf DB.`, res);
+                }
 
-            smurfs.push(newSmurf);
-            smurfId++;
-            res.json(smurfs);
+                smurfs.push(newSmurf);
+                smurfId++;
+                res.json(smurfs);
+            } catch (e) {
+                console.error(e)
+            }
         });
 
         server.put('/smurfs/:id', (req, res) => {
-            const {id} = req.params;
-            const {name, age, height} = req.body;
-            const findSmurfById = (smurf) => {
-                return smurf.id === id;
-            };
-            const foundSmurf = smurfs.find(findSmurfById);
-            if (!foundSmurf) {
-                return sendUserError('No Smurf found by that ID', res);
-            } else {
-                if (name) foundSmurf.name = name;
-                if (age) foundSmurf.age = age;
-                if (height) foundSmurf.height = height;
-                res.json(smurfs);
+
+            try {
+                const {id} = req.params;
+                const {name, age, height} = req.body;
+                const findSmurfById = (smurf) => {
+                    return smurf.id === id;
+                };
+                const foundSmurf = smurfs.find(findSmurfById);
+                if (!foundSmurf) {
+                    return sendUserError('No Smurf found by that ID', res);
+                } else {
+                    if (name) foundSmurf.name = name;
+                    if (age) foundSmurf.age = age;
+                    if (height) foundSmurf.height = height;
+                    res.json(smurfs);
+                }
+            } catch (e) {
+                console.error(e)
             }
         });
 
         server.delete('/smurfs/:id', (req, res) => {
-            const {id} = req.params;
-            const foundSmurf = smurfs.find((smurf) => smurf.id === id);
 
-            if (foundSmurf) {
-                const SmurfRemoved = {...foundSmurf};
-                smurfs = smurfs.filter((smurf) => smurf.id !== id);
-                res.status(200).json(smurfs);
-            } else {
-                sendUserError('No smurf by that ID exists in the smurf DB', res);
+            try {
+                const {id} = req.params;
+                const foundSmurf = smurfs.find((smurf) => smurf.id === id);
+
+                if (foundSmurf) {
+                    const SmurfRemoved = {...foundSmurf};
+                    smurfs = smurfs.filter((smurf) => smurf.id !== id);
+                    res.status(200).json(smurfs);
+                } else {
+                    sendUserError('No smurf by that ID exists in the smurf DB', res);
+                }
+            } catch (e) {
+                console.error(e)
             }
         });
 
@@ -81,7 +98,5 @@ exports.handler = async function (event, context) {
         //     if (err) console.log(err);
         //     console.log(`server is listening on port ${port}`);
         // });
-    } catch (e) {
-        console.error(e)
+
     }
-}
